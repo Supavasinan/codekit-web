1edc7b1f0b212a7f934e9d577809291f4babe872
Supavasinan Promdetch <95174704+Supavasinan@users.noreply.github.com>
2025-05-11 23:17:52 +0700

    feat: default theme to dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <QueryProviders>
           <ThemeProvider
             attribute="class"
-            defaultTheme="light"
+            defaultTheme="dark"
             disableTransitionOnChange
           >
             {children}
